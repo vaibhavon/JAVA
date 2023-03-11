0cf41e6971aff4fa1bd635de6d5c9e9548bbcf23
Vaibhav Giradkar <120658296+vaibhavon@users.noreply.github.com>
2023-03-11 16:58:37 +0530

    Create PositiveNegative.java

---
+++ PositiveNegative.java
@@ -0,0 +1,13 @@
+class PositiveNegative
+{
+    public static void main (String[] args) 
+    {
+    int n=0;
+    if (n>0)
+    System.out.println("Number is positive");
+    if (n<0)
+    System.out.println("Number is Negative ");
+    if (n==0)
+    System.out.println("Number is zero ");
+    }
+}
