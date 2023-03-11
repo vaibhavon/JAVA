c56d48b1da0d14fd774ee364dd6cf4f8fd4518d3
Vaibhav Giradkar <120658296+vaibhavon@users.noreply.github.com>
2023-03-11 17:01:49 +0530

    Create Subtraction.java

---
+++ Subtraction.java
@@ -0,0 +1,14 @@
+/******************************************************************************
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+*******************************************************************************/
+
+class Subtraction {
+	public static void main(String[] args) {
+		int a=50,b=48;
+		int res= a-b;
+		System.out.print("subtraction is "+res);
+			}
+}
