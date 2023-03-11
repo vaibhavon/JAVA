0474161f1c0aea0781a8f92c1391e3f05f7eaeba
Vaibhav Giradkar <120658296+vaibhavon@users.noreply.github.com>
2023-03-11 16:45:25 +0530

    Create Dicision.java

---
+++ Dicision.java
@@ -0,0 +1,13 @@
+/******************************************************************************
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+*******************************************************************************/
+class Division {
+	public static void main(String[] args) {
+		int a=50,b=10;
+		int res= a/b;
+		System.out.print("Division is "+res);
+			}
+}
