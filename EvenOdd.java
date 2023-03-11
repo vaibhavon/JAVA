24c3fad937f484a82454e4f0bbf853a40026cac0
Vaibhav Giradkar <120658296+vaibhavon@users.noreply.github.com>
2023-03-11 16:48:07 +0530

    Create EvenOdd.java

---
+++ EvenOdd.java
@@ -0,0 +1,12 @@
+
+class EvenOdd
+{
+    public static void main (String[] args) 
+    {
+    int n=10;
+    if (n%2==0)
+    System.out.println("Number is evan");
+    else
+    System.out.println("Number is Odd");
+    }
+}
