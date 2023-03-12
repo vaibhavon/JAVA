a40f6875c6386780cd0cebfdf943eb657f91b334
Vaibhav Giradkar <120658296+vaibhavon@users.noreply.github.com>
2023-03-12 14:22:28 +0530

    Add files via upload

---
+++ Task2.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+class Task2{
+	public static void main(String[] args) 
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.print("Enter any number: ");
+		int no=sc.nextInt();
+		int rem,sum=0;
+		int no1=no;
+		do{
+                 rem=no1%10;
+                 rem=rem*rem*rem;
+                 sum=sum+rem;
+                 no1=no1/10;
+		}while(no1>0);
+
+		if (sum==no) {
+		 	System.out.print(no+" is Amstrong Number");
+		}else {
+		 	System.out.print(no+" is not a Amstrong Number");
+		}
+	}
+}
