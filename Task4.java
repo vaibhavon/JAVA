fb9a0ee7015a6312a97771b18ff1182e6e8e2354
Vaibhav Giradkar <120658296+vaibhavon@users.noreply.github.com>
2023-03-12 14:27:17 +0530

    Add files via upload

---
+++ Task4.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class Task4{
+	public static void main(String[] args)
+	{
+		int i=1;
+		do 
+		{
+			System.out.println("\tVaibhav\n ");
+			i++;
+		}while(i<=1000);
+	}
+}
