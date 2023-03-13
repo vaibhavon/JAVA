cf1cff94994829748a6338577ddf1f4ac55a98d3
Vaibhav Giradkar <120658296+vaibhavon@users.noreply.github.com>
2023-03-14 00:19:01 +0530

    Add files via upload

---
+++ PracticeG15 (1).java
@@ -0,0 +1,11 @@
+class PracticeG15
+{
+public static void main(String args[])
+{
+    float f=0.7f;
+    if (f==0.7)
+      System.out.println("Hello");
+    else   
+      System.out.println("Hiii");
+}
+}
