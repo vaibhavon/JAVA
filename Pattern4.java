8b7811a589a6638412cec0f421fb541ea973612a
Vaibhav Giradkar <120658296+vaibhavon@users.noreply.github.com>
2023-03-13 23:52:40 +0530

    Add files via upload

---
+++ Pattern4.java
@@ -0,0 +1,12 @@
+class Pattern4{
+	public static void main(String[] args) {
+		int n=1;
+		for (int i=1;i<=4 ;++i) {
+			for (int j=1;j<=4 ;j++) {
+				System.out.print(n);
+				n++;
+			}
+			System.out.println();
+		}
+	}
+}
