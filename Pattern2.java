02080281b33488105fbce47847ead17b4d87bace
Vaibhav Giradkar <120658296+vaibhavon@users.noreply.github.com>
2023-03-13 23:48:49 +0530

    Add files via upload

---
+++ Pattern2.java
@@ -0,0 +1,10 @@
+class Pattern2{
+	public static void main(String[] args) {
+		for (int i=1;i<=4;i++ ) {
+			for (int j=1;j<i+1 ;j++ ) {
+				System.out.print(j);
+			}
+			System.out.println();
+		}
+	}
+}
