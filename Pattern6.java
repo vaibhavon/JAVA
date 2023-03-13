b0dc5a2fab0c4a998e95af541b13c35d86fed611
Vaibhav Giradkar <120658296+vaibhavon@users.noreply.github.com>
2023-03-13 23:57:41 +0530

    Add files via upload

---
+++ Pattern6.java
@@ -0,0 +1,12 @@
+class Pattern6{
+	public static void main(String[] args) {
+		int n=65;
+		for (int i=1;i<=5 ;i++) {
+			for (int j=1;j<=4 ;j++) {
+				System.out.print((char)n);
+				n++;
+			}
+			System.out.println();
+		}
+	}
+}
