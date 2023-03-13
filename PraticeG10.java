c7f54c658383c65d6d3fb51d54a47595de718b60
Vaibhav Giradkar <120658296+vaibhavon@users.noreply.github.com>
2023-03-14 00:01:50 +0530

    Add files via upload

---
+++ PraticeG10.java
@@ -0,0 +1,12 @@
+class PracticeG10
+{
+public static void main(String args[])
+{
+    double d=200.06;
+    long l=(long)d;
+    int i=(int)l;
+System.out.println("Double data type value is "+d);
+System.out.println("long data tyap value is "+l);
+System.out.println("int data type value is "+i);
+}
+}
