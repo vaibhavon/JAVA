f9ea495c9529d9c27693605887f6a8b984c5ca1f
Vaibhav Giradkar <120658296+vaibhavon@users.noreply.github.com>
2023-03-14 00:07:33 +0530

    Add files via upload

---
+++ PracticeG11.java
@@ -0,0 +1,14 @@
+class PracticeG11
+{
+public static void main(String args[])
+{
+    byte b= 52;
+    short s=1508;
+    int i=548154661;
+    long l=616181855;
+System.out.println(b);
+System.out.println(s);
+System.out.println(i);
+System.out.println(l);
+}
+}
