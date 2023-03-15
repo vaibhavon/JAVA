690d64f0895b82cb042d2364a5a379286c6e840a
Vaibhav Giradkar <120658296+vaibhavon@users.noreply.github.com>
2023-03-16 00:31:51 +0530

    Add files via upload

---
+++ Looping1.java
@@ -0,0 +1,17 @@
+
+class Looping1{
+	public static void main(String[] args) {
+		System.out.println("The Prime Numbers Between 1-100 are: ");
+			for (int i=2;i<=100 ;i++ ) {
+			int count=0;
+			for (int j=1;j<=i ;j++ ) {
+				if (i%j==0) {
+					count++;
+				}
+			}
+			if (count==2) {
+				System.out.println(i+"  ");
+			}
+		}
+	} 
+}
