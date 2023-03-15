85dd7fe17a0a1a930e7f1819fff0a697e67a8914
Vaibhav Giradkar <120658296+vaibhavon@users.noreply.github.com>
2023-03-16 00:34:53 +0530

    Add files via upload

---
+++ Selection4.java
@@ -0,0 +1,17 @@
+/******/
+
+import java.util.Scanner;
+
+class Selection4{
+	public static void main(String[] args) {
+		Scanner in=new Scanner(System.in);
+		System.out.print("Enter any Number: ");
+		int n=in.nextInt();
+			  if (n>0)
+    System.out.println("Number is positive");
+    if (n<0)
+    System.out.println("Number is Negative ");
+    if (n==0)
+    System.out.println("Number is zero ");
+    }
+}
