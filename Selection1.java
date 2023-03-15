b05c8e52f6b4e0cc74ef42b4c61c0d9b7fed8d6e
Vaibhav Giradkar <120658296+vaibhavon@users.noreply.github.com>
2023-03-16 00:34:10 +0530

    Add files via upload

---
+++ Selection1.java
@@ -0,0 +1,16 @@
+/*****/
+import java.util.Scanner;
+
+class Selection1
+{
+	public static void main(String[] args)
+	{
+		Scanner in=new Scanner(System.in);
+		System.out.print("Enter any Number ");
+		int n=in.nextInt();
+     if (n%2==0)
+     System.out.println("Number is evan");
+     else
+     System.out.println("Number is Odd");
+    }
+}
